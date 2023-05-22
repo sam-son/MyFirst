dad250b0570cec1abebb38d5992b786dddf85524
Oleg_Sam <drosa1@mail.ru>
2023-05-22 14:32:47 +1200

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Oleg Samoylov on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
